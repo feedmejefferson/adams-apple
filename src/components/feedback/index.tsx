3d7136a434da429ae035c0b760b4b5bf0e67f333
Jeff Hansen <dscheffy@gmail.com>
2019-07-11 19:09:20 +0200

    Add feedback link to prodpad feedback portal

---
+++ src/components/feedback/index.tsx
@@ -0,0 +1,13 @@
+import { h } from "preact";
+import { connect } from "unistore/preact";
+import { actions } from "../../state"
+import { UserConsent } from "../../state/types";
+import * as style from "./style.css";
+
+export const Feedback = () => 
+<a class={style.feedback} href="https://portal.prodpad.com/571e82d6-a3ac-11e9-a984-0288f735e5b9">Got Feedback?</a>
+
+
+        
+
+
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,11 +2,13 @@ import { Component, h } from "preact";
 import { Link } from "preact-router";
 import { connect } from "unistore/preact";
 import { actions } from "../../state"
+import { Feedback } from "../feedback";
 import * as style from "./style.css";
 
 export const Header = connect([], actions)(({startOver}: any) =>         
 <header class={style.header}>
 <h1><Link href="/">Adams 🍎</Link></h1>
+<Feedback />
 </header>
 )
 
