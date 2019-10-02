e6dd665a84d8c9369ab791ca06877bbfba75d72e
Jeff Hansen <dscheffy@gmail.com>
2019-10-02 15:06:36 +0200

    Build karens kitchen theme with feed me jefferson assets
    It's time to start reskinning adams apple with the feedme jefferson
    theme, but before we go live with it we'll need to test that the whole
    theming functionality actually works correctly. Eventually we'll
    probably have a few different themes that are easily identifiable and
    use them as our various testing environments -- so that nobody ever
    mistakenly thinks they're looking at the wrong environment. The old
    feedme-stage environment has already become something of a legacy
    administrative content curation environment, so we can't reuse that as
    our new staging environment, instead I've created a new firebase app
    called karens kitchen and I'm using it to work on the feedme jefferson
    theme brand assets. When we're happy with it we can rename it to the
    feedme jefferson theme and replace it with some new karens kitchen
    themed assets. This also provided a good opportunity to go through the
    setup of a new environment from scratch and test out/update all of the
    various notes.

--- preact.config.js
+++ preact.config.js
@@ -1,8 +1,17 @@
 import { resolve } from "path";
 import CopyWebpackPlugin from "copy-webpack-plugin";
 import Dotenv from "dotenv-webpack";
+import * as dotenv from "dotenv";
 
 export default function(config, env, helpers) {
+    // before anything, load the theme specific configuration
+    const theme = env.theme ? env.theme : "adams-apple";
+    config.resolve.alias["theme"] = env.source(`themes/${theme}`);
+    const lifeCycle = env.isProd ? env.lifeCycle : "dev";
+    const lifeCycleTheme = lifeCycle ? theme + "." + lifeCycle : theme;
+    dotenv.config({ path: `src/themes/${theme}.env` });
+    dotenv.config({ path: `src/themes/${lifeCycleTheme}.env` });
+
     // Switch css-loader for typings-for-css-modules-loader, which is a wrapper
     // that automatically generates .d.ts files for loaded CSS
     helpers.getLoadersByName(config, "css-loader").forEach(({ loader }) => {
@@ -16,27 +25,30 @@ export default function(config, env, helpers) {
         });
     });
 
+    // Set the html page title to the theme specific site name
+    helpers
+        .getPluginsByName(config, "HtmlWebpackPlugin")
+        .forEach(({ plugin }) => {
+            plugin.options.title = process.env.SITE_NAME;
+        });
+
     // Use any `index` file, not just index.js
     config.resolve.alias["preact-cli-entrypoint"] = resolve(
         process.cwd(),
         "src",
         "index"
     );
-    const theme = env.theme ? env.theme : "adams-apple";
-    config.resolve.alias["theme"] = env.source(`themes/${theme}`);
 
-    const lifeCycle = env.isProd ? env.lifeCycle : "dev";
     const copyList = [
         { from: "themes/common" },
         { from: `themes/${theme}`, force: true }
     ];
+    // right now this really only works for adams apple basket/images
     if (!env.isProd) {
         copyList.push({ from: "themes/dev" });
     }
-
     config.plugins.push(new CopyWebpackPlugin(copyList));
 
-    const lifeCycleTheme = lifeCycle ? theme + "." + lifeCycle : theme;
     // first pull from the main production configuration (as the default)
     // but then apply any lifecycle specific overrides
     config.plugins.push(
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -9,8 +9,11 @@ const siteName = process.env.SITE_NAME;
 
 export const Header = connect([], actions)(({startOver}: any) =>         
 <header class={style.header}>
-<h1><Link href="/">{siteName}</Link></h1>
-<Feedback />
+  <h1><Link href="/">
+    <img class={style.smallScreen} src='/assets/icons/logo.svg' alt="Site Logo" />
+    <img class={style.largeScreen} src='/assets/icons/banner.svg' alt="Site Banner" />
+  </Link></h1>
+  {/* <Feedback /> */}
 </header>
 )
 
