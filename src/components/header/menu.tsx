0bdaca2626521fb9e0e278d82e29b77942430fcf
Jeff Hansen <dscheffy@gmail.com>
2020-08-19 17:21:36 +0200

    Remove feedback link to prodpad portal
    We discontinued our subscription to prodpad, so if we want to support a
    feedback option, we'll either have to build something simple ourselves
    or come up with some other alternative -- until then I'm just removing
    the link to the nonexistent feedback page.

--- src/components/header/menu.tsx
+++ src/components/header/menu.tsx
@@ -10,8 +10,6 @@ interface State {
   checked: boolean;
 }
 
-const FEEDBACK_PORTAL_LINK = process.env.FEEDBACK_PORTAL_LINK;
-
 export class Menu extends Component<Props, State> {
   public render({ }: Props, { checked }: State) {
     return <nav role="navigation">
@@ -24,7 +22,6 @@ export class Menu extends Component<Props, State> {
       <ul class={style.menu}>
         <li><Link onClick={(e)=>{e.preventDefault();this.dismiss();}} href="/">Start Over</Link></li>
         <li><Link onClick={(e)=>{e.preventDefault();this.dismiss();say(step1)}} href="/intro/1">Tutorial</Link></li>
-        <li><a onClick={(e)=>{this.dismiss()}} href={FEEDBACK_PORTAL_LINK} target="_blank" rel="noopener">Feedback</a></li>
       </ul>
     </div>
   </nav>
